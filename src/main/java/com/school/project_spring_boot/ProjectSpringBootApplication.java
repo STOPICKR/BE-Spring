656a01f6fec5a7d4a53ec40de01f927f82f19ffb
YunJae00 <0109548@naver.com>
2024-09-11 02:27:25 +0900

    swagger cors 문제 해결 시도

--- src/main/java/com/school/project_spring_boot/ProjectSpringBootApplication.java
+++ src/main/java/com/school/project_spring_boot/ProjectSpringBootApplication.java
@@ -1,8 +1,12 @@
 package com.school.project_spring_boot;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableScheduling;
+
+@OpenAPIDefinition(servers = {@Server(url = "https://api.trandtrader.com", description = "Default Server URL")})
 
 @SpringBootApplication
 @EnableScheduling
