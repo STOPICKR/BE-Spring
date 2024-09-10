7f1d547eea01944d13088769616eb94e20bcbd47
YunJae00 <0109548@naver.com>
2024-09-11 02:10:19 +0900

    cors config 수정 시도

--- src/main/java/com/school/project_spring_boot/config/CorsConfig.java
+++ src/main/java/com/school/project_spring_boot/config/CorsConfig.java
@@ -10,7 +10,7 @@ public class CorsConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("https://trandtrader.com", "http://api.trandtrader.com", "http://your-swagger-domain.com", "https://your-swagger-domain.com") // API 서버와 Swagger UI가 있는 도메인 허용
+                .allowedOrigins("https://trandtrader.com", "http://trandtrader.com", "https://api.trandtrader.com", "http://api.trandtrader.com", "https://api.trandtrader.com/swagger-ui/index.html", "http://api.trandtrader.com/swagger-ui/index.html") // API 서버와 Swagger UI가 있는 도메인 허용
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // 허용할 HTTP 메서드
                 .allowedHeaders("*") // 허용할 HTTP 헤더
                 .allowCredentials(true); // 클라이언트가 인증 정보를 포함하도록 허용
